a4a5918bf2f9d9b594168bff56b3ff181d4df17c
Meowoverflow <mohammedessammousa@gmail.com>
2021-05-22 11:32:00 -0400

    hotfix process cpuUsage (now it works)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "ncurses_display.h"
 #include "system.h"
-// test
+
 int main() {
   System system;
+  //
+  setenv("TERM", "xterm-256color", 0);
+  std::string s = getenv("TERM");
   NCursesDisplay::Display(system);
 }
--- src/processor.cpp
+++ src/processor.cpp
@@ -3,21 +3,21 @@
 #include "linux_parser.h"
 
 using std::string;
-// TODO: Return the aggregate CPU utilization
+
 float Processor::Utilization() {
-  string line;
+  string line , cpu;
   float utilization;
   long user , nice , systemTime , idle , iowait , irq ,softirq , steal , guest , guestNice , idleTotla , nonIdleTotal , total;
   std::ifstream filestream(LinuxParser::kProcDirectory + LinuxParser::kStatFilename);
   if (filestream.is_open()) {
     getline(filestream, line);
     std::istringstream linestream(line);
-    linestream >> user >> nice >> systemTime >> idle >> iowait >> irq >> softirq >> steal >> guest >> guestNice ;
+    linestream >> cpu >> user >> nice >> systemTime >> idle >> iowait >> irq >> softirq >> steal >> guest >> guestNice ;
     idleTotla = idle + iowait;
     nonIdleTotal = user + nice + systemTime + irq + softirq + steal ;
     total = idleTotla + nonIdleTotal;
-    utilization = (total - idleTotla)/total;
-    return utilization * 1.0;
+    utilization = (total - idleTotla) * 1.0 /total;
+    return utilization;
   }
   return 0.0;
 }
